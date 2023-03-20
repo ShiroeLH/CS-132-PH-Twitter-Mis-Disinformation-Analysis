4ae76f7ac6a464853b25841b51d8cc5d9adafb6c
ShiroeLH <royalcrown112400@gmail.com>
2023-03-20 16:38:02 +0800

    dataset (123)

--- tweets/search.py
+++ tweets/search.py
@@ -3,6 +3,7 @@ import tweepy, csv, sys, re, openpyxl
 import pandas as pd
 import sched, time
 import datetime
+import xlsxwriter
 
 import config
 import preprocess
@@ -11,6 +12,10 @@ import pprint
 # Open the dataset workbook
 workbook = openpyxl.load_workbook(filename = 'tweets/datasets/Dataset - Group 32 datasheet.xlsx')
 worksheet = workbook['Data']
+
+# Write to another workbook
+workbook2 = xlsxwriter.Workbook('tweets/datasets/tweets_dataset.xlsx')
+worksheet2 = workbook2.add_worksheet()
 
 # Collect status IDs
 statuses = []
@@ -21,12 +26,12 @@ status_ids = [int(status.split('/')[-1]) for status in statuses]
 # Open/create a file to append data to and use csv writer
 csvFile = open('tweets/datasets/tweets_dataset.csv', 'a', newline='', encoding='utf-8')
 csvWriter = csv.writer(csvFile)
-csvWriter.writerow([
-                    'TIMESTAMP', 'COLLECTOR', 'TOPIC', 'KEYWORDS', 'ACCOUNT HANDLE',
-                    'ACCOUNT NAME', 'ACCOUNT BIO', 'JOINED', 'FOLLOWING', 'FOLLOWERS',
-                    'LOCATION', 'RAW DATA', 'TRANSLATED DATA', 'DATE POSTED', 'TWEET URL',
-                    'CONTENT TYPE', 'FAVORITES', 'REPLIES', 'RETWEETS',
-                ])
+# csvWriter.writerow([
+#                     'TIMESTAMP', 'COLLECTOR', 'TOPIC', 'KEYWORDS', 'ACCOUNT HANDLE',
+#                     'ACCOUNT NAME', 'ACCOUNT BIO', 'JOINED', 'FOLLOWING', 'FOLLOWERS',
+#                     'LOCATION', 'RAW DATA', 'TRANSLATED DATA', 'DATE POSTED', 'TWEET URL',
+#                     'CONTENT TYPE', 'FAVORITES', 'RETWEETS',
+#                 ])
 
 # Authentication
 auth = tweepy.OAuth2AppHandler(
@@ -40,29 +45,41 @@ collector   = 'Rey Christian E. Delos Reyes'
 keywords    = '(UPD OR PUP OR DLSU OR Ateneo) AND (Komunista OR NPA OR Elitista) until:2023-01-01 since:2019-01-01'
 places      = api.search_geo(query="Philippines", granularity="country"); place_id = places[0].id
 
+# Write preliminary data
+headers = [
+    'TIMESTAMP', 'COLLECTOR', 'TOPIC', 'KEYWORDS', 'ACCOUNT HANDLE',
+    'ACCOUNT NAME', 'ACCOUNT BIO', 'JOINED', 'FOLLOWING', 'FOLLOWERS',
+    'LOCATION', 'RAW DATA', 'TRANSLATED DATA', 'DATE POSTED', 'TWEET URL',
+    'CONTENT TYPE', 'FAVORITES', 'RETWEETS',
+]
+
+for index, id in enumerate(headers):
+    worksheet2.write(0, index, id)
+
 # Get all statuses
-for id in status_ids:
+for index, id in enumerate(status_ids):
     tweet = api.get_status(id, tweet_mode='extended')
     content_type = ', '.join(['text']+[entity for entity in tweet.entities.keys() if len(tweet.entities[entity])])
 
-    csvWriter.writerow([
-                    datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S %p"),                           # TIMESTAMP
-                    collector,                                                                          # COLLECTOR
-                    topic,                                                                              # TOPIC
-                    keywords,                                                                           # KEYWORDS
-                    '@' + tweet.user.screen_name,                                                       # ACCOUNT HANDLE
-                    tweet.user.name,                                                                    # ACCOUNT NAME
-                    tweet.user.description,                                                             # ACCOUNT BIO
-                    tweet.user.created_at,                                                              # JOINED
-                    tweet.user.friends_count,                                                           # FOLLOWING
-                    tweet.user.followers_count,                                                         # FOLLOWERS
-                    places[0].full_name,                                                                # LOCATION
-                    tweet.full_text,                                                                    # RAW DATA
-                    preprocess.simplify_text(tweet.full_text),                                          # TRANSLATED DATA
-                    tweet.created_at,                                                                   # DATE POSTED
-                    'https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str,        # TWEET URL
-                    content_type,                                                                       # CONTENT TYPE 
-                    tweet.favorite_count,                                                               # FAVORITES 
-                    tweet.in_reply_to_status_id_str,                                                    # REPLIES
-                    tweet.retweet_count,                                                                # RETWEETS
-                ])
+    worksheet2.write(index+1, 0, datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S %p"))
+    worksheet2.write(index+1, 1, collector)
+    worksheet2.write(index+1, 2, topic)
+    worksheet2.write(index+1, 3, keywords)
+    worksheet2.write(index+1, 4, '@' + tweet.user.screen_name)
+    worksheet2.write(index+1, 5, tweet.user.name)
+    worksheet2.write(index+1, 6, tweet.user.description)
+    worksheet2.write(index+1, 7, tweet.user.created_at.strftime("%d/%m/%Y %H:%M:%S %p"))
+    worksheet2.write(index+1, 8, tweet.user.friends_count)
+    worksheet2.write(index+1, 9, tweet.user.followers_count)
+    worksheet2.write(index+1, 10, places[0].full_name)
+    worksheet2.write(index+1, 11, tweet.full_text)
+    worksheet2.write(index+1, 12, preprocess.simplify_text(tweet.full_text))
+    worksheet2.write(index+1, 13, tweet.created_at.strftime("%d/%m/%Y %H:%M:%S %p"))
+    worksheet2.write(index+1, 14, 'https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str)
+    worksheet2.write(index+1, 15, content_type)
+    worksheet2.write(index+1, 16, tweet.favorite_count)
+    # worksheet2.write(index+1, 17, tweet.in_reply_to_status_id_str)
+    worksheet2.write(index+1, 17, tweet.retweet_count)
+
+
+workbook2.close()
